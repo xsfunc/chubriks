4e06ffe8e13a8ef361032a0858b850fa07e2d56a
xsfunc <xsfunc@gmail.com>
2023-07-26 12:27:53 +0300

    chore: update

--- src/entities/patterns/ui/pattern-cross.jsx
+++ src/entities/patterns/ui/pattern-cross.jsx
@@ -1,6 +1,6 @@
 import { useUnit } from 'effector-react'
 import { patternsModel } from '../model'
-import { SliderWithLabel, TargetHandle } from '@/shared/ui'
+import { SliderWithLabel } from '@/shared/ui'
 
 export function CrossPattern({ id, data }) {
   const { updatePattern } = useUnit(patternsModel)
@@ -26,22 +26,6 @@ export function CrossPattern({ id, data }) {
       value={data.strokeWidth}
       onChange={onChange('strokeWidth')}
       options={{ type: 'range', min: 1, max: 9 }}
-    />
-
-    <TargetHandle
-      sx={{ mt: 1 }}
-      name='Background: color'
-      options={{
-        id: 'background',
-        isConnectable: true,
-      }}
-    />
-    <TargetHandle
-      name='Waves: color'
-      options={{
-        id: 'color1',
-        isConnectable: true,
-      }}
     />
   </>
 }
--- src/widgets/pattern-node/ui.jsx
+++ src/widgets/pattern-node/ui.jsx
@@ -49,7 +49,6 @@ export function PatternNode({ id, data }) {
           <AddPatternButton nodeId={id} />
         </Card>
       }
-
     </>
   )
 }
