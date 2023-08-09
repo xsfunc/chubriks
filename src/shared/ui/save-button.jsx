2b510876daddfcd6cc9ff208b1f36d25ce68809c
xsfunc <xsfunc@gmail.com>
2023-08-09 20:12:46 +0300

    feat: add tooltip

--- src/shared/ui/save-button.jsx
+++ src/shared/ui/save-button.jsx
@@ -1,15 +1,17 @@
-import { IconButton } from '@mui/joy'
+import { IconButton, Tooltip } from '@mui/joy'
 import RemoveIcon from '~icons/bi/save'
 
 export function SaveButton({ onClick }) {
-  return <IconButton
-    onClick={onClick}
-    aria-label="save"
-    className='nodrag'
-    variant="plain"
-    color="neutral"
-    size="sm"
-  >
-    <RemoveIcon />
-  </IconButton>
+  return <Tooltip title="Download image" variant="outlined">
+    <IconButton
+      onClick={onClick}
+      aria-label="save"
+      className='nodrag'
+      variant="plain"
+      color="neutral"
+      size="sm"
+    >
+      <RemoveIcon />
+    </IconButton>
+  </Tooltip>
 }
